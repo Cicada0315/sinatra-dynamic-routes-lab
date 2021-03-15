54cc850a01fdf0a69d7b2eac24d47de1bed3fdb4
jean <jean.lee.8282@gmail.com>
2021-03-15 17:44:53 -0400

    Done.

--- app.rb
+++ app.rb
@@ -32,7 +32,7 @@ class App < Sinatra::Base
       "#{params[:number1].to_i-params[:number2].to_i}"
     elsif params[:operation]== 'multiply'
       "#{params[:number1].to_i*params[:number2].to_i}"
-    else params[:operation]== 'divide'
+    elsif params[:operation]== 'divide'
       "#{params[:number1].to_i/params[:number2].to_i}"
     end
   end
